02cfb80df2ce6e0b3984b3944588e002231dd7f5
Abhinav Prakash <abhinav.prakash319@gmail.com>
2025-03-11 09:59:15 +0530

    fix::> used enum for better management of string ops
    Signed-off-by: Abhinav Prakash <abhinav.prakash319@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,7 @@ mod server;
 use config::Config;
 use constants::*;
 use std::collections::HashMap;
+use std::str::FromStr;
 use std::{
     env, fs,
     os::unix::prelude::FileTypeExt,
@@ -41,6 +42,7 @@ use client::run_client;
 use logger::init_logger;
 use server::run_server;
 
+#[derive(Debug)]
 struct ClientConfig {
     log_file: String,
     address: String,
@@ -49,88 +51,132 @@ struct ClientConfig {
     dependencies: String,
 }
 
+#[derive(Debug, Clone, PartialEq)]
+enum ActionType {
+    PreStreamer,
+    PreDump,
+    PostDump,
+    PreRestore,
+    Other,
+}
+
+impl ActionType {
+    fn to_str(&self) -> &'static str {
+        match self {
+            ActionType::PreStreamer => ACTION_PRE_STREAMER,
+            ActionType::PreDump => ACTION_PRE_DUMP,
+            ActionType::PostDump => ACTION_POST_DUMP,
+            ActionType::PreRestore => ACTION_PRE_RESTORE,
+            ActionType::Other => "",
+        }
+    }
+}
+
+impl FromStr for ActionType {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            ACTION_PRE_STREAMER => Ok(ActionType::PreStreamer),
+            ACTION_PRE_DUMP => Ok(ActionType::PreDump),
+            ACTION_POST_DUMP => Ok(ActionType::PostDump),
+            ACTION_PRE_RESTORE => Ok(ActionType::PreRestore),
+            _ => Ok(ActionType::Other),
+        }
+    }
+}
+
 const CONFIG_KEY_ID: &str = "id";
 const CONFIG_KEY_DEPS: &str = "dependencies";
 const CONFIG_KEY_ADDR: &str = "address";
 const CONFIG_KEY_PORT: &str = "port";
 const CONFIG_KEY_LOG: &str = "log-file";
 
-fn load_config_file<P: AsRef<Path>>(images_dir: P) -> ClientConfig {
+fn load_config_file<P: AsRef<Path>>(images_dir: P) -> Result<ClientConfig, String> {
     let images_dir = images_dir.as_ref();
-    let mut config_file = images_dir.join(Path::new(CONFIG_FILE));
-    if !config_file.is_file() {
-        // The following allows us to load global config files from /etc/criu.
-        // This is useful for example when we want to use the same config file
-        // for multiple containers.
-        let config_dir = PathBuf::from("/etc/criu");
-        config_file = config_dir.join(Path::new(CONFIG_FILE));
-        if !config_file.is_file() {
-            panic!("config file does not exist")
-        }
-    }
+    // The following allows us to load global config files from /etc/criu.
+    // This is useful for example when we want to use the same config file
+    // for multiple containers.
+    let config_paths = vec![
+        images_dir.join(Path::new(CONFIG_FILE)),
+        PathBuf::from("/etc/criu").join(Path::new(CONFIG_FILE)),
+    ];
+
+    let config_file = config_paths
+        .iter()
+        .find(|path| path.is_file())
+        .ok_or_else(|| "Config file DNE".to_string())?;
 
     let settings = Config::builder()
-        .add_source(config::File::from(config_file))
+        .add_source(config::File::from(config_file.as_path()))
         .build()
         .unwrap();
+
     let settings_map = settings
         .try_deserialize::<HashMap<String, String>>()
-        .unwrap();
+        .map_err(|e| format!("Failed to deserialize config: {}", e))?;
 
-    if !settings_map.contains_key(CONFIG_KEY_ID) {
-        panic!("id missing in config file")
-    }
-    let id = settings_map.get(CONFIG_KEY_ID).unwrap();
+    let id = settings_map
+        .get(CONFIG_KEY_ID)
+        .ok_or_else(|| "ID missing in config file".to_string())?
+        .clone();
 
-    let mut dependencies = String::new();
-    if settings_map.contains_key(CONFIG_KEY_DEPS) {
-        dependencies = settings_map.get(CONFIG_KEY_DEPS).unwrap().to_string();
-    }
+    let dependencies = settings_map
+        .get(CONFIG_KEY_DEPS)
+        .cloned()
+        .unwrap_or_default();
 
-    let mut address = DEFAULT_ADDRESS;
-    if settings_map.contains_key(CONFIG_KEY_ADDR) {
-        address = settings_map.get(CONFIG_KEY_ADDR).unwrap();
-    }
+    let address = settings_map
+        .get(CONFIG_KEY_ADDR)
+        .cloned()
+        .unwrap_or_else(|| DEFAULT_ADDRESS.to_string());
 
-    let mut port = DEFAULT_PORT;
-    if settings_map.contains_key(CONFIG_KEY_PORT) {
-        port = settings_map.get(CONFIG_KEY_PORT).unwrap();
-    }
+    let port = settings_map
+        .get(CONFIG_KEY_PORT)
+        .cloned()
+        .unwrap_or_else(|| DEFAULT_PORT.to_string());
 
-    let mut log_file = "-";
-    if settings_map.contains_key(CONFIG_KEY_LOG) {
-        log_file = settings_map.get(CONFIG_KEY_LOG).unwrap();
-    }
+    let log_file = settings_map
+        .get(CONFIG_KEY_LOG)
+        .cloned()
+        .unwrap_or_else(|| "-".to_string());
 
-    ClientConfig {
-        log_file: log_file.to_string(),
-        address: address.to_string(),
-        port: port.to_string(),
-        id: id.to_string(),
+    Ok(ClientConfig {
+        log_file,
+        address,
+        port,
+        id,
         dependencies,
-    }
+    })
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     if let Ok(action) = env::var(ENV_ACTION) {
-        let images_dir = PathBuf::from(
-            env::var(ENV_IMAGE_DIR)
-                .unwrap_or_else(|_| panic!("Missing {} environment variable", ENV_IMAGE_DIR)),
-        );
+        let images_dir = env::var(ENV_IMAGE_DIR)
+            .map(PathBuf::from)
+            .map_err(|_| format!("Missing {} environment variable", ENV_IMAGE_DIR))?;
 
-        let client_config = load_config_file(&images_dir);
+        let client_config = match load_config_file(&images_dir) {
+            Ok(config) => config,
+            Err(e) => {
+                eprintln!("Error loading config: {}", e);
+                exit(1);
+            }
+        };
 
         // Ignore all action hooks other than "pre-stream", "pre-dump" and "pre-restore".
-        let enable_streaming = match action.as_str() {
-            ACTION_PRE_STREAMER => true,
-            ACTION_PRE_DUMP => {
+        let action_type = action.parse::<ActionType>().unwrap_or(ActionType::Other);
+        let enable_streaming = match action_type {
+            ActionType::PreStreamer => true,
+            ActionType::PreDump => {
                 match fs::symlink_metadata(images_dir.join(IMG_STREAMER_CAPTURE_SOCKET_NAME)) {
                     Ok(metadata) => {
                         if !metadata.file_type().is_socket() {
-                            panic!(
+                            eprintln!(
                                 "{} exists but is not a Unix socket",
                                 IMG_STREAMER_CAPTURE_SOCKET_NAME
                             );
+                            exit(1);
                         }
                         // If the stream socket exists, ignore CRIU's "pre-dump" action hook.
                         exit(0);
@@ -138,9 +184,8 @@ fn main() {
                     Err(_) => false,
                 }
             }
-            ACTION_POST_DUMP => false,
-            ACTION_PRE_RESTORE => false,
-            _ => exit(0),
+            ActionType::PostDump | ActionType::PreRestore => false,
+            ActionType::Other => exit(0),
         };
 
         init_logger(Some(&images_dir), client_config.log_file);
@@ -150,7 +195,7 @@ fn main() {
             client_config.port.parse().unwrap(),
             &client_config.id,
             &client_config.dependencies,
-            &action,
+            &action_type.to_str(),
             &images_dir,
             enable_streaming,
         );
@@ -190,4 +235,5 @@ fn main() {
             run_server(&address, port);
         }
     };
+    Ok(())
 }
